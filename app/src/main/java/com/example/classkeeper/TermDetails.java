11d87d2eea3d1203a49def98c4624b57015b4d06
Zachary Bennett <zbennett10@hotmail.com>
2018-04-05 21:12:41 -0700

    added toolbar and delete icon

--- app/src/main/java/com/example/classkeeper/TermDetails.java
+++ app/src/main/java/com/example/classkeeper/TermDetails.java
@@ -1,14 +1,20 @@
 package com.example.classkeeper;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class TermDetails extends AppCompatActivity {
 
@@ -20,6 +26,9 @@ public class TermDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.TermDetailsAppBar);
+        setSupportActionBar(toolbar);
 
         termID = getIntent().getIntExtra("TERM_ID", 0);
         currentTerm = DatabaseQueryBank.getTerm(TermDetails.this, termID);
@@ -58,6 +67,31 @@ public class TermDetails extends AppCompatActivity {
                 startActivity(intentToViewUpdatedTerm);
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_term_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem selectedItem) {
+        switch (selectedItem.getItemId()) {
+            case R.id.DeleteTermIcon:
+                ArrayList<Course> termCourses = DatabaseQueryBank.getAllCoursesWithTermID(TermDetails.this, termID);
+                if(termCourses.size() < 1) {
+                    DatabaseQueryBank.deleteTermByID(TermDetails.this, termID);
+                } else {
+                    View rootView = TermDetails.this.getWindow().getDecorView().findViewById(R.id.TermDetailsContent);
+                    Snackbar unableToDeleteSnack = Snackbar.make(rootView, "Unable to delete - courses are attached to this term.", Snackbar.LENGTH_SHORT);
+                    unableToDeleteSnack.show();
+                }
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(selectedItem);
+
+        }
     }
 }
